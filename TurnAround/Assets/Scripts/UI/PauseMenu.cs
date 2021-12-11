14f615efd0888742d296965d7bc05ae7d8989e78
eFrancoisCHING <francois.ching@epitech.eu>
2021-12-12 00:23:16 +0900

    [Add] main menu, pause menu prefab, settings prefab

---
+++ TurnAround/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MainMenu
+{
+    [SerializeField] private GameObject m_leaveMenu;
+    [SerializeField] private GameObject m_panel;
+    // [SerializeField] private GameObject m_gameOverMenu;
+
+    private bool m_inPauseMenu = false;
+
+    private void Update() {
+        // if (!m_gameOverMenu.activeSelf && Input.GetKeyDown("esc")) {
+        if (Input.GetKeyDown("escape")) {
+            Debug.Log("esc");
+            if (!m_inPauseMenu) {
+                pause();
+            } else {
+                resume();
+            }
+        }
+    }
+
+    private void pause() {
+        Time.timeScale = 0f;
+        m_panel.SetActive(true);
+        m_inPauseMenu = true;
+    }
+
+    public void resume() {
+        closeSettingsMenu();
+        closeLeaveMenu();
+        Time.timeScale = 1f;
+        m_panel.SetActive(false);
+        m_inPauseMenu = false;
+    }
+
+    public void openLeaveMenu() {
+        m_mainMenu.SetActive(false);
+        m_leaveMenu.SetActive(true);
+    }
+
+    public void closeLeaveMenu() {
+        m_mainMenu.SetActive(true);
+        m_leaveMenu.SetActive(false);
+    }
+
+    public void goToMainMenu() {
+        resume();
+        SceneManager.LoadScene(0);
+    }
+}
